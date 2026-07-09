f411dcc10c68f264922f04024d5c9f4ff86c923d
ANKIT <sycocoders@gmail.com>
2026-07-09 18:01:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from ai_video import generate_video
 
 app = Flask(__name__)
 
@@ -16,6 +17,24 @@ def dashboard():
 @app.route("/history")
 def history():
     return render_template("history.html")
+
+
+@app.route("/generate", methods=["POST"])
+def generate():
+
+    prompt = request.form.get("prompt")
+
+    try:
+        video = generate_video(prompt)
+
+        return render_template(
+            "dashboard.html",
+            prompt=prompt,
+            video=video
+        )
+
+    except Exception as e:
+        return f"Error: {e}"
 
 
 if __name__ == "__main__":
