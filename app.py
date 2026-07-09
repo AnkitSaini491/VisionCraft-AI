e102763702e2833415d0ee885f9ebc3a65c5f9a1
ANKIT <sycocoders@gmail.com>
2026-07-09 17:52:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/history")
+def history():
+    return render_template("history.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
