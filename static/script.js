3b62df2a1c9113e6dc4b84848bd03f199fabdb3a
ANKIT <sycocoders@gmail.com>
2026-07-09 18:05:20 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,16 @@
 
+const form = document.querySelector("form");
+
+if(form){
+
+form.addEventListener("submit",function(){
+
+const btn = document.querySelector("button");
+
+btn.innerHTML="⏳ Generating...";
+
+btn.disabled=true;
+
+});
+
+}
